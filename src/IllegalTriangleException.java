fe1454a38ba337dbb4a054c9a534f6e8033f1e88
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-01-05 11:19:11 +0700

    IllegalTriangleException

---
+++ src/IllegalTriangleException.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IllegalTriangleException {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        try {
+            System.out.println("Enter three side of triangle");
+            int side1=scanner.nextInt();
+            int side2=scanner.nextInt();
+            int side3=scanner.nextInt();
+            checkLegalTriangle(side1,side2,side3);
+            System.out.println("three sides of triangle is "+side1+" , "+side2+" , "+side3);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void checkLegalTriangle(int side1, int side2,int side3) throws Exception{
+        boolean isLegalTriangle=(side1+side2)>side3&&(side1+side3)>side2&&(side2+side3)>side1;
+        if (!isLegalTriangle) throw new Exception("there is illegal triangle");
+    }
+}
